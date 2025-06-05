d57f8d2b3624acc82f09a4d97970eb01366d83d8
eduardo2382 <motadossantosjoseeduardo.m82@gmail.com>
2025-06-05 18:57:34 -0300

    adicionado arquivos

---
+++ faculdade/haskell/exercicioAula.hs
@@ -0,0 +1,12 @@
+media :: Int -> Int -> Int -> Int
+media n1 n2 n3 = (n1+n2+n3)/3
+
+acimaMedia :: Int -> Bool
+acimaMedia 
+    
+
+verificarNums :: Int -> Int -> Int -> Int
+verificarNums n1 n2 n3 = 
+    where
+        quant = 0
+
