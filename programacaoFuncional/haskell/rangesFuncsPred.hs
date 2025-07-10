c9af81c6de2c5bb3d47493bbd3af86005901bf10
eduardo2382 <motadossantosjoseeduardo.m82@gmail.com>
2025-07-10 20:04:29 -0300

    letras a) e c) da 3

--- programacaoFuncional/haskell/rangesFuncsPred.hs
+++ programacaoFuncional/haskell/rangesFuncsPred.hs
@@ -1,19 +1,34 @@
 --1.
 --a)
-['a'..'z'] ++ ['A'..'Z']
+-- ['a'..'z'] ++ ['A'..'Z']
 
 --b)
-[102, 105..1000]
+-- [102, 105..1000]
 
 --c)
-[999, 996..102]
+-- [999, 996..102]
 
 --d)
-[102, 105..1000] ++ [100, 105..200]
+-- [102, 105..1000] ++ [100, 105..200]
 
 
 --3.
 --a)
-head (tail [1,2,3])
+segundoElemento :: [Int] -> Int
+segundoElemento lista = head (tail lista)
 
 --b)
+atePonto :: String -> String
+atePonto lista 
+    |head lista == '.' =
+
+digitosDecimais :: Float -> String
+digitosDecimais num = lista
+    where
+        lista = show num
+
+
+
+--c)
+enesimoElemento :: [Int] -> Int -> Int
+enesimoElemento lista pos = head(drop (pos-1) lista)
