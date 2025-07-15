68ca75d5288eb812e9a82eaf3ea91b978db76c1a
eduardo2382 <motadossantosjoseeduardo.m822gmail.com>
2025-07-15 11:47:12 -0300

    exercicio 4 de compreensao

--- programacaoFuncional/haskell/listasCompreensoes.hs
+++ programacaoFuncional/haskell/listasCompreensoes.hs
@@ -9,3 +9,7 @@ imparTri lista = [x*3 | x <- lista, mod x 2 /= 0]
 quantosPar :: [Int] -> Int
 quantosPar lista = sum [1 | x <- lista, mod x 2 == 0]
 
+--4
+divisores :: Int -> [Int]
+divisores num = [x | x <- [1..num], mod num x == 0]
+
