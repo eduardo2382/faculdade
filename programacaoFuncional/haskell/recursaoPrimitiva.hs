e78fed074e819a2f73d4be4cf14ea4659998e668
eduardo2382 <motadossantosjoseeduardo.m822gmail.com>
2025-08-01 16:16:46 -0300

    exercicio 2 de recursao primitiva

--- programacaoFuncional/haskell/recursaoPrimitiva.hs
+++ programacaoFuncional/haskell/recursaoPrimitiva.hs
@@ -2,3 +2,8 @@
 multiplica :: Int -> Int -> Int
 multiplica num 1 = num
 multiplica num cont = num + (multiplica num (cont-1))
+
+--2
+elevado :: Int -> Int -> Int
+elevado m 1 = m
+elevado m n = m * (elevado m (n-1))
