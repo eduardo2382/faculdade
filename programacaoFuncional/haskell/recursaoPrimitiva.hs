0f4d853e76c8e3f16bee7dba07acd7995b2068d7
eduardo2382 <motadossantosjoseeduardo.m822gmail.com>
2025-08-01 17:01:36 -0300

    exercicio 5 de recursao primitiva

--- programacaoFuncional/haskell/recursaoPrimitiva.hs
+++ programacaoFuncional/haskell/recursaoPrimitiva.hs
@@ -5,10 +5,25 @@ multiplica num cont = num + (multiplica num (cont-1))
 
 --2
 elevado :: Int -> Int -> Int
+elevado m 0 = 1
 elevado m 1 = m
 elevado m n = m * (elevado m (n-1))
 
 --3 
-elevado3 :: Int -> Int
-elevado3 0 = 0
-elevado3 n = elevado n 3 + (elevado3 (n-1))
+elevadoTres :: Int -> Int
+elevadoTres 0 = 0
+elevadoTres n = elevado n 3 + (elevadoTres (n-1))
+
+--4
+doisElevado :: Int -> Int
+doisElevado 0 = 1
+doisElevado n = elevado 2 n + doisElevado (n-1)
+
+--5
+serie :: Float -> Float
+serie 0 = 1/1
+serie n = 1/(fatorial n) + serie (n-1)
+    where
+        fatorial 0 = 1
+        fatorial 1 = 1
+        fatorial num = num*(fatorial (num-1))
